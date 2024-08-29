c86a082492aa115dd20d8e2ee7f9bd15b861e418
notmax6677 <max.meln1210@gmail.com>
2024-08-29 14:17:38 +0200

    removed files that were generated specifically for my computer to allow other systems to build/run

--- cmake/CMakeFiles/mc_clone.dir/include/NOISE/noise1234.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/mc_clone.dir/include/NOISE/noise1234.c.o: \
- /home/max/Projects/c/mc_clone/include/NOISE/noise1234.c \
- /usr/include/stdc-predef.h \
- /home/max/Projects/c/mc_clone/include/NOISE/noise1234.h
